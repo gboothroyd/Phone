4c22e21ef9ef179d4f6bdd29dfc706c47a405cca
Noah Chung <nmchung@dmacc.edu>
2021-10-11 22:46:28 -0500

    ServiceProvider and Helper
    I updated ServiceProvider, formatted a little bit of phone, and implemented the ServiceProviderHelper.

---
+++ src/controller/ServiceProviderHelper.java
@@ -0,0 +1,57 @@
+package controller;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.NoResultException;
+import javax.persistence.Persistence;
+import javax.persistence.TypedQuery;
+
+import model.ServiceProvider;
+
+/**
+ * @author Noah Chung - nmchung
+ * CIS175 - Fall 2021
+ * Oct 11, 2021
+ */
+public class ServiceProviderHelper {
+	static EntityManagerFactory emfactory = Persistence.createEntityManagerFactory("Phone");
+	
+	public void insertServiceProvider(ServiceProvider sp) {
+		EntityManager em = emfactory.createEntityManager();
+		em.getTransaction().begin();
+		em.persist(sp);
+		em.getTransaction().commit();
+		em.close();
+	}
+	
+	public List<ServiceProvider> showAllServiceProviders() {
+		EntityManager em = emfactory.createEntityManager();
+		List<ServiceProvider> allServiceProviders = em.createQuery("SELECT sp FROM ServiceProvider sp").getResultList();
+		return allServiceProviders;
+	}
+
+	/**
+	 * @param developerName
+	 * @return
+	 */
+	public ServiceProvider findServiceProvider(String nameToLookUp) {
+		// TODO Auto-generated method stub
+		EntityManager em = emfactory.createEntityManager();
+		em.getTransaction().begin();
+		TypedQuery<ServiceProvider> typedQuery = em.createQuery("select prov from ServiceProvider prov where prov.serviceProviderName = :selectedName", ServiceProvider.class);
+		typedQuery.setParameter("selectedName", nameToLookUp);
+		typedQuery.setMaxResults(1);
+		
+		ServiceProvider foundServiceProvider;
+		try {
+			foundServiceProvider = typedQuery.getSingleResult();
+		} catch (NoResultException ex) {
+			foundServiceProvider = new ServiceProvider(nameToLookUp);
+		}
+		em.close();
+		
+		return foundServiceProvider;
+	}
+}
--- src/model/ServiceProvider.java
+++ src/model/ServiceProvider.java
@@ -1,15 +1,52 @@
 package model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
 /**
  * @author booth - gboothroyd@dmacc.edu
  *CIS175 - Fall 2021
  * Oct 6, 2021
  */
+@Entity
+@Table(name="service_providers")
 public class ServiceProvider {
-private String name;
-private int yearEstablished;
+	@Id
+	@GeneratedValue
+	private int id;
+	private String name;
+	private int yearEstablished;
+	
 	public ServiceProvider() {
 		// TODO Auto-generated constructor stub
+	}
+	
+	public ServiceProvider(int id, String name, int yearEstablished) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.yearEstablished = yearEstablished;
+	}
+	
+	public ServiceProvider(String name, int yearEstablished) {
+		super();
+		this.name = name;
+		this.yearEstablished = yearEstablished;
+	}
+
+	public ServiceProvider(String name) {
+		super();
+		this.name = name;
+	}
+
+	public int getId() {
+		return id;
+	}
+	
+	public void setId(int id) {
+		this.id = id;
 	}
 	/**
 	 * @return the name
@@ -35,7 +72,9 @@ private int yearEstablished;
 	public void setYearEstablished(int yearEstablished) {
 		this.yearEstablished = yearEstablished;
 	}
-	
-	
 
+	@Override
+	public String toString() {
+		return "ServiceProvider [id=" + id + ", name=" + name + ", yearEstablished=" + yearEstablished + "]";
+	}
 }
